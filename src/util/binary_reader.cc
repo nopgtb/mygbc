893668241071c252ba981bd673b6e4f75b695808
NopGTB <nopgtb@gmail.com>
2025-04-25 20:28:10 +0300

    Moved BinaryReader to use StatusOr

--- src/util/binary_reader.cc
+++ src/util/binary_reader.cc
@@ -4,15 +4,19 @@
 /// @brief Reads the file and returns its contents as std::vector
 /// @details Reads the file at the given path and returns the contents as std::vector<uint8_t>. If the file cannot be read throws std::ifstream::failure.
 /// @param file_path The file path to the file.
-/// @throws std::ifstream::failure If the file cannot be read (e.g. if the file does not exists).
 /// @return A vector of uint8_t representing the file contents in binary format.
-std::vector<uint8_t> BinaryReader::read_as_bytes(const std::string& file_path){
+StatusOr<std::vector<uint8_t>> BinaryReader::read_as_bytes(const std::string& file_path) noexcept{
     std::ifstream binary_file(file_path, std::ios::binary);
     //Is file accesible?
     if(!binary_file){
-        throw std::ifstream::failure("File at the given path was not accesible!");
+        return Status::io_error("File at the given path was not accesible!");
     }
     //Read and return file contents
-    std::vector<uint8_t> buffer(std::istreambuf_iterator<char>(binary_file), {});
-    return buffer;
+    try{
+        std::vector<uint8_t> buffer(std::istreambuf_iterator<char>(binary_file), {});
+        return buffer;
+    }
+    catch(const std::bad_alloc& e){
+        return Status::io_error(std::string("Issues with allocating memory! ") + e.what());
+    }
 }
--- src/util/binary_reader.h
+++ src/util/binary_reader.h
@@ -1,6 +1,7 @@
 #ifndef BINARY_READER_H
 #define BINARY_READER_H
 
+#include "status/status_or.h" //StatusOr
 #include <vector> //std::vector
 #include <string> //std::string
 #include <cstdint> //Fixed lenght variables
@@ -10,12 +11,11 @@
 class BinaryReader{
     public:
     
-        /// @brief Reads the file and returns its contents as std::vector
-        /// @details Reads the file at the given path and returns the contents as std::vector<uint8_t>. If the file cannot be read throws std::ifstream::failure.
+        /// @brief Reads the file and returns its contents as std::vector or error status.
+        /// @details Reads the file at the given path and returns the contents as std::vector<uint8_t>. If the file cannot be read return error status.
         /// @param file_path The file path to the file.
-        /// @throws std::ifstream::failure If the file cannot be read (e.g. if the file does not exists).
-        /// @return A vector of uint8_t representing the file contents in binary format.
-        static std::vector<uint8_t> read_as_bytes(const std::string& file_path);
+        /// @return A vector of uint8_t representing the file contents in binary format or error status.
+        static StatusOr<std::vector<uint8_t>> read_as_bytes(const std::string& file_path) noexcept;
 };
 
 #endif
