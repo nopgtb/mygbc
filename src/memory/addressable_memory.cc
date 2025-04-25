f65fb8f06fee5fca1574f6ad8022409d80175b53
NopGTB <nopgtb@gmail.com>
2025-04-25 21:40:35 +0300

    Moved addressable_memory to use Status and StatusOr

--- src/memory/addressable_memory.cc
+++ src/memory/addressable_memory.cc
@@ -1,11 +1,10 @@
 #include "addressable_memory.h" //AddressableMemory
 #include "../util/util.h" //Util
-#include <stdexcept> //std::out_of_range
 #include <cstring> //std::memcpy
 
 /// @brief Default constructor
 /// @details Default constructor, empty memory, sets read only flag to false.
-AddressableMemory::AddressableMemory():read_only_memory_(false){
+AddressableMemory::AddressableMemory():read_only_memory_(false) {
 }
 
 /// @brief Setter constructor, sets read only flag to false.
@@ -24,46 +23,46 @@ AddressableMemory::~AddressableMemory(){
 /// @brief Flips the read only memory flag value.
 /// @param flag New read only flag value.
 /// @details Flips the read only memory flag value.
-void AddressableMemory::set_read_only_flag(bool flag){
+void AddressableMemory::set_read_only_flag(bool flag) noexcept{
     read_only_memory_ = flag;
 }
 
 /// @brief Returns the read only memory flag.
 /// @details Returns the read only memory flag.
 /// @return The read only memory flag.
-bool AddressableMemory::is_read_only() const{
+bool AddressableMemory::is_read_only() const noexcept{
     return read_only_memory_;
 }
 
 /// @brief Returns the byte located at the given address.
 /// @details Returns the byte located at the given address. Address is zero-based indexed. 
 /// @param addr Zero based address.
-/// @return byte value located at the given address.
-/// @throw std::out_of_range if given address is not valid address. 
-uint8_t AddressableMemory::get_byte(const uint16_t addr) const {
+/// @return byte value located at the given address or error Status.
+StatusOr<uint8_t> AddressableMemory::get_byte(const uint16_t addr) const noexcept{
     if(addr < memory_.size()){
         return memory_[addr];
     }
-    else{
-        throw std::out_of_range("Invalid address given! Can't access memory at address.");
-    }
+    return Status::invalid_index_error(
+        "Invalid address given for byte read! Can't access memory at address(Addr: " + 
+        std::to_string(addr) + "/ Limit: " + std::to_string(memory_.size()) + ")."
+    );
 }
 
 /// @brief Returns the word located at the given address.
 /// @details Returns the word located at the given address. Address is zero-based indexed. 
 /// @param addr Zero based address.
-/// @return Word value located at the given address.
-/// @throw std::out_of_range if given address is not valid address. 
-uint16_t AddressableMemory::get_word(const uint16_t addr) const{
+/// @return Word value located at the given address or error Status.
+StatusOr<uint16_t> AddressableMemory::get_word(const uint16_t addr) const noexcept{
     const uint16_t byte_one_addr = addr;
     const uint16_t byte_two_addr = addr + 1;
     if(byte_two_addr < memory_.size()){
         uint16_t val = (static_cast<uint16_t>(memory_[byte_two_addr]) << 8) | static_cast<uint16_t>(memory_[byte_one_addr]);
         return Util::nthos16_t(val);
     }
-    else{
-        throw std::out_of_range("Invalid address given! Can't access memory at address.");
-    }
+    return Status::invalid_index_error(
+        "Invalid address given for word read! Can't access memory at address(Addr: " + 
+        std::to_string(addr) + "/ Limit: " + std::to_string(memory_.size()) + ")."
+    );
 }
 
 /// @brief Allows read only access to the whole memory.
@@ -77,29 +76,27 @@ const std::vector<uint8_t>& AddressableMemory::get_memory() const{
 /// @details Set the byte located at the given address to the given value. Address is zero-based indexed. 
 /// @param addr Zero based address.
 /// @param value Byte, New value.
-/// @throw std::out_of_range if given address is not valid address. 
-/// @throw std::logic_error if protected memory flag is set
-void AddressableMemory::set(const uint16_t addr, const uint8_t value){
+/// @return Returns status of the set
+Status AddressableMemory::set(const uint16_t addr, const uint8_t value) noexcept{
     if(addr < memory_.size()){
         if(!is_read_only()){
             memory_[addr] = value;
+            return Status::ok_status();
         }
-        else{
-            throw std::logic_error("Can't write on protected memory!");
-        }
+        return Status::protected_memory_set_error("Tried to set memory when is_read_only flag was set!");
     }
-    else{
-        throw std::out_of_range("Invalid address given! Can't access memory at address.");
-    }
+    return Status::invalid_index_error(
+        "Invalid address given for byte write! Can't access memory at address(Addr: " + 
+        std::to_string(addr) + "/ Limit: " + std::to_string(memory_.size()) + ")."
+    );
 }
 
 /// @brief Sets the word located at the given address to the given value.
 /// @details Set the word located at the given address to the given value. Address is zero-based indexed. 
 /// @param addr Zero based address.
 /// @param value Word, New value.
-/// @throw std::out_of_range if given address is not valid address.
-/// @throw std::logic_error if protected memory flag is set
-void AddressableMemory::set(const uint16_t addr, const uint16_t value){
+/// @return Returns status of the set
+Status AddressableMemory::set(const uint16_t addr, const uint16_t value) noexcept{
     const uint16_t first_byte_addr = addr;
     const uint16_t second_byte_addr = addr + 1;
     if(second_byte_addr < memory_.size()){
@@ -108,27 +105,26 @@ void AddressableMemory::set(const uint16_t addr, const uint16_t value){
             const uint8_t * byte_ptr = reinterpret_cast<const uint8_t*>(&value);
             memory_[first_byte_addr] = byte_ptr[1];
             memory_[second_byte_addr] = byte_ptr[0];
+            return Status::ok_status();
         }
-        else{
-            throw std::logic_error("Can't write on protected memory!");
-        }
+        return Status::protected_memory_set_error("Tried to set memory when is_read_only flag was set!");
     }
-    else{
-        throw std::out_of_range("Invalid address given! Can't access memory at address.");
-    }
+    return Status::invalid_index_error(
+        "Invalid address given for word write! Can't access memory at address(Addr: " + 
+        std::to_string(addr) + "/ Limit: " + std::to_string(memory_.size()) + ")."
+    );
 }
 
 /// @brief Sets the contents of the memory to the given value. Observes read_only flag.
 /// @details Sets the contents of the memory to the given value. Observes read_only flag.
 /// @param contents new contents of the memory
-/// @throw std::logic_error if read_only flag is set. 
-void AddressableMemory::set_memory(const std::vector<uint8_t>& contents){
+/// @return Returns status of the set
+Status AddressableMemory::set_memory(const std::vector<uint8_t>& contents) noexcept{
     if(!is_read_only()){
         memory_ = contents;
+        return Status::ok_status();
     }
-    else{
-        throw std::logic_error("Tried to set memory that is protected by a read only flag!");
-    }
+    return Status::protected_memory_set_error("Tried to set total memory when is_read_only flag was set!");
 }
 
 /// @brief Frees the memory assosiated with the memory object.
--- src/memory/addressable_memory.h
+++ src/memory/addressable_memory.h
@@ -3,6 +3,8 @@
 
 #include <vector> //std::vector
 #include <cstdint> //Fixed lenght variables
+#include "status/status_or.h" //StatusOr
+#include "status/status.h" //Status
 
 /// @brief Interface class for all addressable memory.
 /// @details Interface class for all addressable memory. Contains functionality for read only memory.
@@ -26,16 +28,14 @@ class AddressableMemory{
     /// @brief Returns the byte located at the given address.
     /// @details Returns the byte located at the given address. Address is zero-based indexed. 
     /// @param addr Zero based address.
-    /// @return byte value located at the given address.
-    /// @throw std::out_of_range if given address is not valid address. 
-    virtual uint8_t get_byte(const uint16_t addr) const;
+    /// @return byte value located at the given address or error Status.
+    virtual StatusOr<uint8_t> get_byte(const uint16_t addr) const noexcept;
 
     /// @brief Returns the word located at the given address.
     /// @details Returns the word located at the given address. Address is zero-based indexed. 
     /// @param addr Zero based address.
-    /// @return Word value located at the given address.
-    /// @throw std::out_of_range if given address is not valid address. 
-    virtual uint16_t get_word(const uint16_t addr) const;
+    /// @return Word value located at the given address or error Status.
+    virtual StatusOr<uint16_t> get_word(const uint16_t addr) const noexcept;
 
     /// @brief Allows read only access to the whole memory.
     /// @details Returns const reference to the memory.
@@ -46,23 +46,21 @@ class AddressableMemory{
     /// @details Set the byte located at the given address to the given value. Address is zero-based indexed. 
     /// @param addr Zero based address.
     /// @param value Byte, New value.
-    /// @throw std::out_of_range if given address is not valid address.
-    /// @throw std::logic_error if protected memory flag is set
-    virtual void set(const uint16_t addr, const uint8_t value);
+    /// @return Returns status of the set
+    virtual Status set(const uint16_t addr, const uint8_t value) noexcept;
 
     /// @brief Sets the word located at the given address to the given value.
     /// @details Set the word located at the given address to the given value. Address is zero-based indexed. 
     /// @param addr Zero based address.
     /// @param value Word, New value.
-    /// @throw std::out_of_range if given address is not valid address. 
-    /// @throw std::logic_error if protected memory flag is set
-    virtual void set(const uint16_t addr, const uint16_t value);
+    /// @return Returns status of the set
+    virtual Status set(const uint16_t addr, const uint16_t value) noexcept;
 
     /// @brief Sets the contents of the memory to the given value. Observes read_only flag.
     /// @details Sets the contents of the memory to the given value. Observes read_only flag.
     /// @param contents new contents of the memory
-    /// @throw std::logic_error if read_only flag is set. 
-    virtual void set_memory(const std::vector<uint8_t>& contents);
+    /// @return Returns status of the set
+    virtual Status set_memory(const std::vector<uint8_t>& contents) noexcept;
 
     /// @brief Frees the memory assosiated with the memory object.
     /// @details Frees the memory assosiated with the memory object.
@@ -71,12 +69,12 @@ class AddressableMemory{
     /// @brief Flips the read only memory flag value.
     /// @param flag New read only flag value.
     /// @details Flips the read only memory flag value.
-    void set_read_only_flag(bool flag);
+    void set_read_only_flag(bool flag) noexcept;
 
     /// @brief Returns the read only memory flag.
     /// @details Returns the read only memory flag.
     /// @return The read only memory flag.
-    bool is_read_only() const;
+    bool is_read_only() const noexcept;
 
     protected:
         //Binary bytes
