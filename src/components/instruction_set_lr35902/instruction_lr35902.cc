e96de585adfe093b0521a575190faee8683d6b08
NopGTB <nopgtb@gmail.com>
2025-04-27 19:10:20 +0300

    Added LR35902 instruction

---
+++ src/components/instruction_set_lr35902/instruction_lr35902.cc
@@ -0,0 +1,73 @@
+#include "instruction_lr35902.h"
+
+/// @brief Initializes the struct members
+/// @details Sets up the constants of the structure.
+/// @param reg_id Letter id of the register
+/// @param op_pos_asm Position of the operand (Assembly)
+/// @param is_address Value is to be treated as address
+/// @param dec_post_op Decrement register after operation is done
+/// @param inc_post_op Increment register after operation is done
+/// @param val_op_mod Value of register is modified by the read value before op
+InstructionLR35902::OperandRegister::OperandRegister(const std::string& reg_id, const uint8_t op_pos_asm, const bool is_address, const bool dec_post_op, const bool inc_post_op, const bool val_op_mod)
+:id(reg_id), operand_position(op_pos_asm), address_mode(is_address), decrement(dec_post_op), increment(inc_post_op), value_operand_modified(val_op_mod){
+}
+
+/// @brief Initializes the struct members for constant operand
+/// @param val Value of the constant operand
+/// @param op_pos_asm Position of the operand in the asm instruction 
+InstructionLR35902::OperandConstValue::OperandConstValue(const uint8_t val, const uint8_t op_pos_asm)
+:value(val), operand_position(op_pos_asm){
+}
+
+/// @brief Initializes a invalid InstructionLR35902.
+/// @details Uses the gap at 0xED.
+InstructionLR35902::InstructionLR35902()
+:opcode(0xED), size_in_bytes(0), operand_registers(), operand_const_values(), has_read_value(false),
+read_value_size_in_bytes(0), read_value_operand_position(0), read_value_operand_interp_hint(InstructionLR35902::OperandValueInterpHint::NONE),
+execution_condition(InstructionLR35902::ExecutionCondition::NONE), short_mnemonic("ILLEGAL"), full_mnemonic("ILLEGAL"), t_cycles_to_execute(0), t_cycles_to_not_execute(0),
+effect_on_flag_z(InstructionLR35902::FlagOperation::NO_CHANGE), effect_on_flag_n(InstructionLR35902::FlagOperation::NO_CHANGE), 
+effect_on_flag_h(InstructionLR35902::FlagOperation::NO_CHANGE), effect_on_flag_c(InstructionLR35902::FlagOperation::NO_CHANGE){
+}
+
+/// @brief Initializes the constant values of the instruction
+/// @param op Opcode 1-2 bytes long
+/// @param byte_size Size of the whole instruction (including read values)
+/// @param oper_reg Operand registers
+/// @param oper_const Operand constants
+/// @param has_r_val Has read value following opcode
+/// @param r_val_size Size of the read value
+/// @param r_val_pos Position of the read value asm
+/// @param r_val_interp_hint How to interpert the read value
+/// @param exec_cond Condition of the execution
+/// @param s_mnem Short ASM mnemonic, e.g. LD
+/// @param f_mnem Full ASM mnemonic 
+/// @param t_cyc_exec Cost in cycles to execute the instruction
+/// @param t_cyc_not_exec Cost in cycles to not execute the instruction
+/// @param eff_flag_z Effect on Zero flag
+/// @param eff_flag_n Effect on Subtract flag 
+/// @param eff_flag_h Effect on Half carry flag
+/// @param eff_flag_c Effect on Carry flag
+InstructionLR35902::InstructionLR35902(
+    const uint16_t op,
+    const uint8_t byte_size,
+    const std::vector<OperandRegister>& oper_reg,
+    const std::vector<OperandConstValue>& oper_const,
+    const bool has_r_val,
+    const uint8_t r_val_size,
+    const uint8_t r_val_pos,
+    const OperandValueInterpHint r_val_interp_hint,
+    const ExecutionCondition exec_cond,
+    const std::string& s_mnem,
+    const std::string& f_mnem,
+    const uint8_t t_cyc_exec,
+    const uint8_t t_cyc_not_exec,
+    const FlagOperation eff_flag_z,
+    const FlagOperation eff_flag_n,
+    const FlagOperation eff_flag_h,
+    const FlagOperation eff_flag_c
+)
+:opcode(op), size_in_bytes(byte_size), operand_registers(oper_reg), operand_const_values(oper_const), has_read_value(has_r_val),
+read_value_size_in_bytes(r_val_size), read_value_operand_position(r_val_pos), read_value_operand_interp_hint(r_val_interp_hint),
+execution_condition(exec_cond), short_mnemonic(s_mnem), full_mnemonic(f_mnem), t_cycles_to_execute(t_cyc_exec), t_cycles_to_not_execute(t_cyc_not_exec),
+effect_on_flag_z(eff_flag_z), effect_on_flag_n(eff_flag_n), effect_on_flag_h(eff_flag_h), effect_on_flag_c(eff_flag_c){
+}
--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <iostream> //std::cout
 #include "util/io/binary_reader.h" //BinaryReader
 #include "memory/gbc_binary.h" //GBCBinary
+#include "components/instruction_set_lr35902/instruction_decoder_lr35902.h" //InstructionDecoderLR35902
 
 int main(int argc, char* argv[]){
     if(argc > 1){
@@ -12,6 +13,15 @@ int main(int argc, char* argv[]){
                 StatusOr<GBCBinary> gbc_binary = GBCBinary::parse_bytes(buffer.value());
                 if(gbc_binary.ok()){
                     std::cout << "Parsed binary successfully!\n\n" << gbc_binary.value().to_string() << "\n";
+                    InstructionSetLR35902 lr35902_instructions;
+                    const uint16_t cartridge_entry_point = 0x100;
+                    StatusOr<InstructionLR35902> instruction = InstructionDecoderLR35902::decode(gbc_binary.value(), cartridge_entry_point, lr35902_instructions);
+                    if(instruction.ok()){
+                        std::cout << "Decoded instruction " << instruction.value().full_mnemonic << " from address " << cartridge_entry_point;
+                    }
+                    else{
+                        std::cout << "Could not decode instruction from address " << cartridge_entry_point << ", got status " << std::to_string(static_cast<int>(instruction.get_status().get_type()));
+                    }
                 }
                 else{
                     std::cout << "Failed to parse as GBCBinary! \n";
