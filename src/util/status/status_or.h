277494afd61e130ccb74f61177534a25a04afa0f
NopGTB <nopgtb@gmail.com>
2025-05-06 20:11:50 +0300

    StatusOr. Removed redundant constructors. Added rvalue and lvalue value getters.

--- src/util/status/status_or.h
+++ src/util/status/status_or.h
@@ -1,9 +1,9 @@
 #ifndef STATUS_OR_H
 #define STATUS_OR_H
 
+#include <concepts> //std::constructible_from, std::is_same
 #include "status.h" //Status
 #include "bad_status_or_access.h" //BadStatusOrAccess
-#include <concepts> //std::constructible_from, std::is_same
 
 namespace mygbc{
 
@@ -11,12 +11,14 @@ namespace mygbc{
     /// @tparam T Base type of the value.
     template <typename T>
     class StatusOr{
+
         public:
+
             /// @brief Default constructor.
             /// @details Builds status unkown.
-            explicit StatusOr():status_(Status::StatusType::UNKOWN, ""), value_(){}
+            explicit StatusOr():status_(Status::StatusType::UNKOWN, ""){}
 
-            /// @brief Copy constructor builds a status ok, statusor with value.
+            /// @brief Constructor builds a status ok, statusor with value.
             /// @details Copies value from val.
             /// @tparam U Expects U to be constructable from T.
             /// @param val Value is copied from val.
@@ -40,7 +42,7 @@ namespace mygbc{
             /// @param val Status is copied from val.
             template <typename U>
             requires std::is_same_v<U, Status>
-            StatusOr(const U& val):status_(val), value_(){
+            StatusOr(const U& val):status_(val){
             }
 
             /// @brief Moves status from given status
@@ -49,58 +51,53 @@ namespace mygbc{
             /// @param val Status is moved from val using std::move.
             template <typename U>
             requires std::is_same_v<U, Status>
-            StatusOr(U&& val):status_(std::move(val)), value_(){
+            StatusOr(U&& val):status_(std::move(val)){
             }
 
-            /// @brief Explicit constructor for status, build statusor with the given status.
-            /// @details Explicit to avoid mistakes in return statements.
-            /// @param status Status of the StatusOr.
-            explicit StatusOr(const Status& status):status_(status), value_(){
-            }
-
-            /// @brief Explicit constructor for status, build statusor with the given status.
-            /// @details Explicit to avoid mistakes in return statements.
-            /// @param status Status of the StatusOr.
-            explicit StatusOr(Status&& status):status_(std::move(status)), value_(){
-            }
-
-            /// @brief Returns the value of the statusor.
-            /// @details Status that is not ok, results in a std::logic_error throw. Check ok() before access!
+            /// @brief Returns the value of the statusor as const reference.
+            /// @details Callable on const lvalue. Status that is not ok, results in a std::logic_error throw. Check ok() before access!
             /// @return Const reference to the value of the statusor.
             /// @throw BadStatusOrAccess if the status is not OK and value is being accessed.
-            T value() const{
+            const T& value() const &{
                 if(ok()){
                     return value_;
                 }
                 throw BadStatusOrAccess("Tried to access value when status was not OK!");
             }
 
-            /// @brief Returns the value of the statusor or given U.
-            /// @details Status that is not ok, results in given U being returned. T must be able to be constructed from U.
-            /// @tparam U Expects U to be constructable from T.
-            /// @return value of the statusor.
-            template <typename U>
-            requires std::constructible_from<T, U>
-            T value_or(U&& dval) const noexcept{
+            /// @brief Returns the value of the statusor as const reference.
+            /// @details Callable on lvalue. Status that is not ok, results in a std::logic_error throw. Check ok() before access!
+            /// @return Const reference to the value of the statusor.
+            /// @throw BadStatusOrAccess if the status is not OK and value is being accessed.
+            T& value() &{
                 if(ok()){
                     return value_;
                 }
-                return std::forward<U>(dval);
+                throw BadStatusOrAccess("Tried to access value when status was not OK!");
             }
 
-            /// @brief Returns the value of the statusor or given U.
-            /// @details Status that is not ok, results in given U being returned. T must be able to be constructed from U.
-            /// @tparam U Expects U to be constructable from T.
-            /// @return value of the statusor.
-            template <typename U>
-            requires std::constructible_from<T, U>
-            T value_or(const U& dval) const noexcept{
+            /// @brief rvalue move return for taking ownership of the statusor value. Invalidates any further value access.
+            /// @details callable on rvalue. uses the std::move(statusor).value() pattern to grant ownership.
+            /// @return StatusOr value with ownership.
+            /// @throw BadStatusOrAccess if the status is not OK and value is being accessed.
+            T&& value() &&{
                 if(ok()){
-                    return value_;
+                    return std::move(value_);
                 }
-                return dval;
+                throw BadStatusOrAccess("Tried to access value when status was not OK!");
             }
-            
+
+            /// @brief rvalue move return for taking ownership of the statusor value. Invalidates any further value access.
+            /// @details callable on const rvalue.
+            /// @return StatusOr value with ownership.
+            /// @throw BadStatusOrAccess if the status is not OK and value is being accessed.
+            const T&& value() const&&{
+                if(ok()){
+                    return std::move(value_);
+                }
+                throw BadStatusOrAccess("Tried to access value when status was not OK!");
+            }
+
             /// @brief Checks wheter the status of the statusor is ok.
             /// @details Calls the ok function of the internal status.
             /// @return const bool value dependent on if the internal status is ok.
@@ -111,7 +108,7 @@ namespace mygbc{
             /// @brief Returns the internal status.
             /// @details Const reference to the internal status object. 
             /// @return internal status object
-            const Status& get_status() noexcept{
+            const Status& status() noexcept{
                 return status_;
             }
 
